4c83ef5db27e743262c6350c49b34bc860c221e0
Emmanuel Osei Owusu <oseiowusue@gmail.com>
2022-07-12 20:34:04 -0700

    stack and queue

--- pstr.c
+++ pstr.c
@@ -1,35 +1,31 @@
 #include "monty.h"
-/**
- * pstr_monty - prints the string starting at the top of the stack
- * @stack: Pointer To The head
- * @line_number: The Line Number
- * Return:Void
- */
-void pstr_monty(stack_t **stack, unsigned int line_number)
-{
-	int i;
-	stack_t *node;
-	(void)line_number;
 
-	node = *stack;
+/**
+ * pstr - Function that prints string of the stack
+ * @stack: stack structure
+ * @line_number: number of instruction
+ */
+void pstr(stack_t **stack, UN unsigned int line_number)
+{
+	stack_t *temp = NULL;
+
 	if (*stack == NULL)
 	{
-		putchar('\n');
+		putchar(10);
+		return;
 	}
-	else
+
+	temp = *stack;
+
+	while (temp)
 	{
-		for (i = 0; node; i++)
+		if (temp->n <= 0 || temp->n >= 128)
 		{
-			if (node->n >= 65 && node->n <= 90)
-				putchar(node->n);
-			else if (node->n >= 97 && node->n <= 122)
-				putchar(node->n);
-			else if (node->n == 0)
-				node = node->next;
-			else
-				break;
-		node = node->next;
+			putchar(10);
+			return;
 		}
-		putchar('\n');
+		printf("%c", temp->n);
+		temp = temp->next;
 	}
+	putchar(10);
 }
